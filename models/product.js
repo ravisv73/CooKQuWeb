f165f445ddf49d796dafdfe086c5103bb1a66738
ravisv73 <ravisv73@gmail.com>
2022-09-29 13:30:17 -0400

    fixed the build issues again

--- models/product.js
+++ models/product.js
@@ -1,25 +1,28 @@
 import mongoose from "mongoose";
 
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    recipeSlug: { type: String, required: true },
+    masalaRecipeSlug: { type: String, required: true },
+    category: { type: String, required: true },
+    image: { type: String, required: true },
+    recipe_url: { type: String, required: true },
+    price: { type: String, required: true },
+    cuisine: { type: String, required: true },
+    rating: { type: String, required: true, default: 0 },
+    numReviews: { type: String, required: true, default: 0 },
+    countInStock: { type: Number, required: true, default: 0 },
+    status: { type: String, required: true, default: "Not Available" },
+    description: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const productSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    slug: {type: String, required: true, unique: true},
-    recipeSlug: {type: String, required: true},    
-    masalaRecipeSlug: {type: String, required: true},
-    category: {type: String, required: true},
-    image: {type: String, required: true},
-    recipe_url: {type: String, required: true},
-    price: {type: String, required: true},
-    cuisine: {type: String, required: true},
-    rating: {type: String, required: true, default: 0},
-    numReviews: {type: String, required: true, default: 0},
-    countInStock: {type: Number, required: true, default: 0},
-    status: {type: String, required: true, default: 'Not Available'},
-    description: {type: String, required: true},
-}, {
-    timestamps: true
-});
-
-const Product = mongoose.models.Product || mongoose.model('Product', productSchema);
+const Product =
+  mongoose.models.Product || mongoose.model("Product", productSchema);
 
 export default Product;
